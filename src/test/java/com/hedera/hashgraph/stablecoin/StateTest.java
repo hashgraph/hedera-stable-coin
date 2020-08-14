e680024b1dd98f15a60d05ac28bbdfdba6d15405
tim <tim@launchbadge.com>
2020-08-13 21:32:24 -0700

    test: add Approve, Constructor, SetKyc, State, UnsetKyc unit tests.  add Mint test, but is unfinished

---
+++ src/test/java/com/hedera/hashgraph/stablecoin/StateTest.java
@@ -0,0 +1,50 @@
+package com.hedera.hashgraph.stablecoin;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+public class StateTest {
+    @Test
+    public void stateTest() {
+        State state = new State();
+
+        //TokenName : String = “”
+        Assertions.assertEquals("", state.getTokenName());
+
+        // TokenSymbol : String = “”
+        Assertions.assertEquals("",state.getTokenSymbol());
+
+        // TokenDecimal : Int = 0
+        Assertions.assertEquals(new BigInteger("0"), state.getTokenDecimal());
+
+        // TotalSupply : Int = 0
+        Assertions.assertEquals(new BigInteger("0"), state.getTotalSupply());
+
+        // Owner : Address = 0x
+        Assertions.assertSame(Address.ZERO, state.getOwner());
+
+        // SupplyManager: Address = {}
+        Assertions.assertNull(state.getSupplyManager());
+
+        // AssetProtectionManager: Address = {}
+        Assertions.assertNull(state.getAssetProtectionManager());
+
+        // Balances: Map::Address->Int = {}
+        Assertions.assertTrue(state.isBalanceEmpty());
+
+        // Allowances: Map::Address->(Map::Address->Int) = {}
+        Assertions.assertTrue(state.isAllowancesEmpty());
+
+        // Frozen: Map::Address->Bool = {}
+        Assertions.assertTrue(state.isFrozenEmpty());
+
+        // KycPassed: Map::Address->Bool = {}
+        Assertions.assertTrue(state.isKycPassedEmpty());
+
+        // ProposedOwner: Address = 0x
+        Assertions.assertSame(Address.ZERO, state.getProposedOwner());
+
+    }
+}
---
+++ src/test/java/com/hedera/hashgraph/stablecoin/UnsetKycPassedTest.java
@@ -0,0 +1,111 @@
+package com.hedera.hashgraph.stablecoin;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import com.hedera.hashgraph.sdk.TopicId;
+import com.hedera.hashgraph.stablecoin.proto.Transaction;
+import com.hedera.hashgraph.stablecoin.transaction.ConstructTransaction;
+import com.hedera.hashgraph.stablecoin.transaction.SetKycPassedTransaction;
+import com.hedera.hashgraph.stablecoin.transaction.UnsetKycPassedTransaction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+public class UnsetKycPassedTest {
+    State state = new State();
+    Client client = Client.forTestnet();
+    TopicListener topicListener = new TopicListener(state, client, new TopicId(1));
+
+    @Test
+    public void unsetKycPassedTest() throws InvalidProtocolBufferException {
+        var callerKey = PrivateKey.generate();
+        var caller2Key = PrivateKey.generate();
+        var addrKey = PrivateKey.generate();
+        var caller = new Address(callerKey.getPublicKey());
+        var caller2 = new Address(caller2Key.getPublicKey());
+        var addr = new Address(addrKey.getPublicKey());
+
+        // prepare state
+        var tokenName = "tokenName";
+        var tokenSymbol = "tokenSymbol";
+        var tokenDecimal = new BigInteger("2");
+        var totalSupply = new BigInteger("10000");
+
+        var constructTransaction = new ConstructTransaction(
+            callerKey,
+            tokenName,
+            tokenSymbol,
+            tokenDecimal,
+            totalSupply,
+            caller,
+            caller2
+        );
+
+        var setKycPassedTransaction = new SetKycPassedTransaction(
+            callerKey,
+            addr
+        );
+
+        topicListener.handleTransaction(Transaction.parseFrom(constructTransaction.toByteArray()));
+        topicListener.handleTransaction(Transaction.parseFrom(setKycPassedTransaction.toByteArray()));
+
+        // prepare test transaction
+        var unsetKycPassedTransaction = new UnsetKycPassedTransaction(
+            callerKey,
+            addr
+        );
+
+        // Pre-Check
+
+        // i. Owner != 0x
+        Assertions.assertTrue(state.hasOwner());
+
+        // ii. caller = AssetProtectionManager || caller = Owner
+        Assertions.assertEquals(caller, state.getOwner());
+
+        // iii. !isPrivilegedRole(addr)
+        Assertions.assertFalse(state.isPrivilegedRole(addr));
+
+        // update State
+        topicListener.handleTransaction(Transaction.parseFrom(unsetKycPassedTransaction.toByteArray()));
+
+        // Post-Check
+
+        // i. !KycPassed[addr]
+        Assertions.assertFalse(state.isKycPassed(addr));
+
+        // re-set and check for caller == AssetProtectionManager instead this time
+        topicListener.handleTransaction(Transaction.parseFrom(setKycPassedTransaction.toByteArray()));
+
+        Assertions.assertTrue(state.isKycPassed(addr));
+
+        var unsetKycPassedTransaction2 = new SetKycPassedTransaction(
+            caller2Key,
+            addr
+        );
+
+        // Update State
+        topicListener.handleTransaction(Transaction.parseFrom(unsetKycPassedTransaction2.toByteArray()));
+
+        // Pre-Check
+
+        // i. Owner != 0x
+        Assertions.assertTrue(state.hasOwner());
+
+        // ii. caller = AssetProtectionManager || caller = Owner
+        Assertions.assertEquals(caller2, state.getAssetProtectionManager());
+
+        // iii. !isPrivilegedRole(addr)
+        Assertions.assertFalse(state.isPrivilegedRole(addr));
+
+        // update State
+        topicListener.handleTransaction(Transaction.parseFrom(unsetKycPassedTransaction.toByteArray()));
+
+        // Post-Check
+
+        // i. !KycPassed[addr]
+        Assertions.assertFalse(state.isKycPassed(addr));
+    }
+}
