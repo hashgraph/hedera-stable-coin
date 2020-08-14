ddef79efdc82554a0bb4b7a7962d419a324e2513
tim <tim@launchbadge.com>
2020-08-14 14:27:57 -0700

    test: add ChangeSupplyManagerTest

---
+++ src/test/java/com/hedera/hashgraph/stablecoin/ChangeSupplyManagerTest.java
@@ -0,0 +1,77 @@
+package com.hedera.hashgraph.stablecoin;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.hedera.hashgraph.sdk.Client;
+import com.hedera.hashgraph.sdk.PrivateKey;
+import com.hedera.hashgraph.sdk.TopicId;
+import com.hedera.hashgraph.stablecoin.proto.Transaction;
+import com.hedera.hashgraph.stablecoin.transaction.ChangeSupplyManagerTransaction;
+import com.hedera.hashgraph.stablecoin.transaction.ConstructTransaction;
+import com.hedera.hashgraph.stablecoin.transaction.SetKycPassedTransaction;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+public class ChangeSupplyManagerTest {
+    State state = new State();
+    Client client = Client.forTestnet();
+    TopicListener topicListener = new TopicListener(state, client, new TopicId(1));
+
+    @Test
+    public void changeSupplyManagerTest() throws InvalidProtocolBufferException {
+        var callerKey = PrivateKey.generate();
+        var addrKey = PrivateKey.generate();
+        var caller = new Address(callerKey.getPublicKey());
+        var addr = new Address(addrKey.getPublicKey());
+
+        // prepare state
+        var tokenName = "tokenName";
+        var tokenSymbol = "tokenSymbol";
+        var tokenDecimal = new BigInteger("2");
+        var totalSupply = new BigInteger("10000");
+
+        var constructTransaction = new ConstructTransaction(
+            callerKey,
+            tokenName,
+            tokenSymbol,
+            tokenDecimal,
+            totalSupply,
+            caller,
+            caller
+        );
+
+        var setKycTransaction = new SetKycPassedTransaction(callerKey, addr);
+
+        topicListener.handleTransaction(Transaction.parseFrom(constructTransaction.toByteArray()));
+        topicListener.handleTransaction(Transaction.parseFrom(setKycTransaction.toByteArray()));
+
+        // prepare test transaction
+        var changeSupplyManagerTransaction = new ChangeSupplyManagerTransaction(
+            callerKey,
+            addr
+        );
+
+        // Pre-Check
+
+        // i. Owner != 0x
+        Assertions.assertFalse(state.getOwner().isZero());
+
+        // ii. caller = Owner
+        Assertions.assertEquals(caller, state.getOwner());
+
+        // iii. addr != 0x
+        Assertions.assertFalse(addr.isZero());
+
+        // iv. CheckTransferAllowed(addr)
+        Assertions.assertTrue(state.checkTransferAllowed(addr));
+
+        // Update State
+        topicListener.handleTransaction(Transaction.parseFrom(changeSupplyManagerTransaction.toByteArray()));
+
+        // Post-Check
+
+        // i. SupplyManager = addr
+        Assertions.assertEquals(addr, state.getSupplyManager());
+    }
+}
