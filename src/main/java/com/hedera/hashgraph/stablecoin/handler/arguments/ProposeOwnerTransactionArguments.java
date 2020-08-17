101ae6e32f4b2a7e686d7eff8036547b9d9bf5b8
tim <tim@launchbadge.com>
2020-08-17 13:22:16 -0700

    refactor: remove unused imports

--- src/main/java/com/hedera/hashgraph/stablecoin/handler/arguments/ProposeOwnerTransactionArguments.java
+++ src/main/java/com/hedera/hashgraph/stablecoin/handler/arguments/ProposeOwnerTransactionArguments.java
@@ -3,8 +3,6 @@ package com.hedera.hashgraph.stablecoin.handler.arguments;
 import com.hedera.hashgraph.stablecoin.Address;
 import com.hedera.hashgraph.stablecoin.proto.TransactionBody;
 
-import java.math.BigInteger;
-
 public final class ProposeOwnerTransactionArguments {
     public final Address address;
 
